647b246e904c315234be0a794b24354828519c8c
mmashiqurrahman <m.m.ashiqur.rahman@gmail.com>
2022-03-20 21:56:58 +0600

    Created migration, products table, ProductController and Product model

---
+++ database/migrations/2022_03_20_154808_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->string('category_name');
+            $table->string('name');
+            $table->string('slug');
+            $table->decimal('price')->nullable();
+            $table->integer('quantity')->default(0);
+            $table->string('image', 500)->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('in_stock')->default(false);
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
 
 /*
 |--------------------------------------------------------------------------
